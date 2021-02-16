284a515c2b5262904051dc5f5b39f263de344176
BuildTools <unconfigured@null.spigotmc.org>
2021-02-16 14:41:50 +0100

    alles

---
+++ c#/Les1CSBO/Les1CSBO/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace Les1CSBO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("H");
+            Thread.Sleep(1000);
+            Console.Write("e");
+            Thread.Sleep(1000);
+            Console.Write("l");
+            Thread.Sleep(1000);
+            Console.Write("l");
+            Thread.Sleep(1000);
+            Console.Write("o");
+            Thread.Sleep(1000);
+            Console.Write(" ");
+            Thread.Sleep(1000);
+            Console.Write("Y");
+            Thread.Sleep(1000);
+            Console.Write("o");
+            Thread.Sleep(1000);
+            Console.Write("u");
+            Thread.Sleep(1000);
+            Console.Write("!");
+            Thread.Sleep(1000);
+        }
+    }
+}
---
+++ c#/les2CS/les2CS/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace les2CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string world = "Hello World!";
+
+            Console.WriteLine(world);
+        }
+    }
+}
