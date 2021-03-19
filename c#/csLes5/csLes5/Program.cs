3378b6eeb8e1c0cad8f90260c24ebc70642fcca2
BuildTools <unconfigured@null.spigotmc.org>
2021-03-19 19:37:01 +0100

    alles van deze week

---
+++ c#/csLes5/csLes5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace csLes5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            eenfunctie();
+
+
+           void eenfunctie()
+           { 
+                if (true)
+                {
+                    Console.WriteLine("wank");
+                }
+
+                {
+                    int scope = 420;
+                    for (int i = 0; i <= 10; i++)
+                    {
+                        Console.WriteLine(scope);
+
+                        if(i == 10)
+                        {
+                            Console.WriteLine("dit is de if: " s+ scope);
+                        }
+                    }
+                }
+           }
+
+        }
+    }
+}
---
+++ c#/csles5scope/csles5scope/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace csles5scope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (true)
+            {
+                // Scope line 8: If Statement & Alles er buiten
+                string scope8Var = "8";
+
+                string scope12Var = "N/A";
+
+                if (true)
+                {
+                    // Scope line 12: If Statement & Alles er buiten
+                    scope12Var = "12";
+                }
+
+                string scope17Var = "N/A";
+
+                if (false)
+                {
+                    // Scope line 17: If Statement & Alles er buiten    
+                    scope17Var = "17";
+                }
+
+                Console.WriteLine("This was set in scope line 8: " + scope8Var);
+                Console.WriteLine("This was set in scope line 12: " + scope12Var);
+                Console.WriteLine("This was set in scope line 17: " + scope17Var);
+            }
+        }
+    }
+}
